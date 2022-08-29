2b718c6a38c298999e0625444fa4f51b29a9d078
philsfootwear <112303474+philsfootwear@users.noreply.github.com>
2022-08-29 08:37:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,9 @@
 window.onload=function(){
     var name = '';
     var submitted = false;
-    document.getElementById('submitButton').addEventListener('click', function(){
-        document.getElementById('bgImgg').width = window.innerWidth;
-        document.getElementById('bgImgg').height = window.innerHeight;
-        if(submitted == false && !(document.getElementById('name').value == "")&& !(document.getElementById('email').value == "")&& !(document.getElementById('addy').value == "")){
-            name = document.getElementById('name').value;
-            alert(`THANK YOU ${name}`);
-            submitted = true;
-        }
-        else if (submitted == true){
-            alert('Reload to submit another form');
-        }
-        else {
-            alert('Invalid Input');
-        }
+    document.getElementById('purchase').addEventListener('click', function(){
+        setTimeout(function(){
+            alert("hi")
+        } ,1000);
     });
 }
